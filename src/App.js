8ef2bfe8d155af5d9a26f9b92f85cf5134803279
IanMacnian <ianschmidt2002@gmail.com>
2024-10-23 17:56:17 -0300

    cambios en styles.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './styles.css';  // Make sure this points to the CSS file you create
+import SalesDashboard from './components/SalesDashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Analitiks SPA - Business Sales Dashboard</h1>
+      <div className="dashboard-container">
+        <SalesDashboard />
+      </div>
     </div>
   );
 }
